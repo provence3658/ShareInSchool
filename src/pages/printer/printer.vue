12f2f718ed147db016f32e0e028b828b844754ef
provence3658 <13386528836@163.com>
2019-03-11 17:57:07 +0800

    xx

--- src/pages/printer/printer.vue
+++ src/pages/printer/printer.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="printer">
     <simple-header :title="title"></simple-header>
+    <div class="content-wrapper">
+      <my-filter></my-filter>
+    </div>
   </div>
 </template>
 
 <script>
 import SimpleHeader from '../commonComponents/SimpleHeader/SimpleHeader'
+import MyFilter from './filter/filter'
 export default {
   name: 'Printer',
   data () {
@@ -14,11 +18,19 @@ export default {
     }
   },
   components: {
-    SimpleHeader
+    SimpleHeader,
+    MyFilter
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.printer
+  .content-wrapper
+    position absolute
+    top 50px
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
 </style>
